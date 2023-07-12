d3de876c882352813be05622f043217ebd1d0d64
Avineesh Kompella <AvineeshKom@gmail.com>
2023-07-12 00:40:28 -0700

    basic movement

---
+++ PIG Summer Jam/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(xMove * speed, rb.velocity.y, zMove * speed);
+    }
+}
