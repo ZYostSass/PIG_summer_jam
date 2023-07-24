b68734a27fd7b3b6df7b82784e4e57c87157ca74
Avineesh Kompella <AvineeshKom@gmail.com>
2023-07-23 21:39:43 -0700

    Updated movement

--- PIG Summer Jam/Assets/Scripts/Player.cs
+++ PIG Summer Jam/Assets/Scripts/Player.cs
@@ -4,195 +4,78 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public float groundDrag;
+
+    [HideInInspector] public float walkSpeed;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
     Rigidbody rb;
 
-    [Header("Movement")]
-    public float speed = 5f;
-    public bool isRunning;
-    public Transform orientation;
-    Vector3 moveDir;
-    Vector3 moveAmount;
-    Vector3 smoothMoveVel;
-
-    [Header("Jumping")]
-    public float jumpForce = 10f;
-    public float raycastDistanceJump = 0.6f;
-    private bool isGrounded;
-    public float groundDrag;
-    private bool canDoubleJump;
-    public LayerMask ground;
-    
-    [Header("Parrying")]
-    private bool canParry;
-    public float raycastDistanceParry = 1f;
-    private Transform parryTarget;
-    public LayerMask weapon;
-
-    [Header("Pickup")]
-    [SerializeField] private LayerMask pickupMask;
-    [SerializeField] private Camera playerCamera;
-    [SerializeField] private Transform pickupTarget;
-    [SerializeField] private float pickupRange;
-    private Rigidbody currentObject;
-
-    [Header("Dialogue")]
-    public bool inDialogue = false;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        //rb.velocity = new Vector3(xSpeed * speed, rb.velocity.y, zSpeed * speed);
-        
-        // Stop movement if in dialogue
-        if (inDialogue) {
-            speed = 0;
-        }
-        else {
-            speed = 5f;
-        }
-        
+        // ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
 
-        // This is for if we want to implement jumping.
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, raycastDistanceJump, ground)) 
-        {
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
-        }
+        MyInput();
+        SpeedControl();
 
-        if (isGrounded) 
-        {
+        // handle drag
+        if (grounded)
             rb.drag = groundDrag;
-        }
-        else 
-        {
-            rb.drag = 0;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space)) 
-        {
-            if (isGrounded)
-            {
-                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-                canDoubleJump = true;
-            }
-            else if (canDoubleJump) 
-            {
-                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-                canDoubleJump = false;
-            }
-        }
-
-        // This is for a parrying weapon mechanic.
-        if (Input.GetMouseButtonDown(0)) 
-        {
-            ParryAttack();
-        }
-
-        // For sprinting.
-        if (Input.GetKey(KeyCode.LeftShift)) 
-        {
-            isRunning = true;
-        }
         else
-        {
-            isRunning = false;
-        }
-
-        ShiftToRun();
-
-        // This is for an item pickup mechanic.
-        if (Input.GetMouseButton(1)) 
-        {
-            if (currentObject)
-            {
-                currentObject.useGravity = true;
-                currentObject = null;
-                return;
-            }
-
-            Ray cameraRay = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-            if (Physics.Raycast(cameraRay, out RaycastHit hitInfo, pickupRange, pickupMask))
-            {
-                currentObject = hitInfo.rigidbody;
-                currentObject.useGravity = false;
-                Debug.Log(currentObject.name);
-            }
-        }
+            rb.drag = 0;
     }
 
-    private void FixedUpdate() 
+    private void FixedUpdate()
     {
-        float xSpeed = Input.GetAxis("Horizontal");
-        float zSpeed = Input.GetAxis("Vertical");
-        
-        moveDir = orientation.forward * zSpeed + orientation.right * xSpeed;
-        Vector3 targetMove = moveDir * speed;
-        moveAmount = Vector3.SmoothDamp(moveAmount, targetMove, ref smoothMoveVel, .15f);
+        MovePlayer();
+    }
 
-        //rb.AddForce(moveDir.normalized * speed, ForceMode.Force);
-        //rb.MovePosition (rb.position + transform.TransformDirection (moveAmount) * Time.fixedDeltaTime);
-        transform.position += moveDir * (speed * Time.deltaTime); 
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
 
-        // This is for an item pickup mechanic.
-        if (currentObject)
+    private void MovePlayer()
+    {
+        // calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        // on ground
+        if(grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity if needed
+        if(flatVel.magnitude > moveSpeed)
         {
-            Vector3 directionToPoint = pickupTarget.position - currentObject.position;
-            float distanceToPoint = directionToPoint.magnitude;
-
-            currentObject.velocity = directionToPoint * 12f * distanceToPoint;
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
-    }
-
-    // This is for a parrying weapon mechanic.
-    void ParryAttack() 
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.forward * raycastDistanceParry, out hit, raycastDistanceParry, weapon))
-        {
-            parryTarget = hit.transform;
-            parryTarget.GetComponent<Rigidbody>().AddForce(Vector3.left, ForceMode.Impulse);
-            Debug.Log(parryTarget.name);
-        }
-    }
-
-    // For sprinting.
-    void ShiftToRun()
-    {
-        if (!inDialogue) 
-        {
-            if (isRunning)
-            {
-                speed = 15f;
-            }
-            else
-            {
-                speed = 5f;
-            }
-        }
-    }
-
-    public void onDialogue() 
-    {
-        inDialogue = true;
-    }
-
-    public void exitDialogue()
-    {
-        inDialogue = false;
-    }
-
-    // For debugging purposes.
-    private void OnDrawGizmos() {
-        Gizmos.DrawRay(transform.position, Vector3.forward * raycastDistanceParry);
     }
 }
